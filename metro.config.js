d3eb9a427daf969829f679089295c63ee7c6150f
Vine <your.email@example.com>
2026-07-08 05:13:18 +0200

    changes some colors

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,4 @@ const { withNativewind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
  
-<<<<<<< HEAD
 module.exports = withNativewind(config, { input: "./src/global.css" });
-=======
-module.exports = withNativewind(config, { input: "./src/global.css" });
->>>>>>> 787ca73e1007f58d3b2cc1b780c5007ab1be117e
