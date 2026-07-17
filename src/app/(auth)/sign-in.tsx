67036712f9d3766a25ba246b948ccb299ba54fb6
Vine-77 <shemadivin7@gmail.com>
2026-07-18 01:46:04 +0200

    your commit message

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+      <Link href="/(auth)/sign-up">Sign In</Link>
+    </View>
+  );
+};
+
+export default SignIn;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,25 @@
-import "@/global.css"
+import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
- 
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Sign Up
+      </Link>
     </View>
   );
 }
