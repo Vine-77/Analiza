8cfbe62169a7dcadc0331003d1c8fcadc1f3129c
Vine-77 <shemadivin7@gmail.com>
2026-07-22 23:25:04 +0200

    makes some changes

---
+++ src/app/(tabs)/subscriptions.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const subscriptions = () => {
+  return (
+    <View>
+      <Text>subscriptions</Text>
+    </View>
+  )
+}
+
+export default subscriptions
